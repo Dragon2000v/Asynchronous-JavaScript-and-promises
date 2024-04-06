524fdb8e6d12268506a8e97715799bbbc22b5d4a
Dragon2000v <shmatko064@gmail.com>
2024-04-06 21:55:14 +0300

    finish

---
+++ src/js/1-timer.js
@@ -0,0 +1,102 @@
+import flatpickr from 'flatpickr';
+import izitoast from 'izitoast';
+const startBtn = document.querySelector('button[data-start]');
+const datetimePicker = document.querySelector('#datetime-picker');
+
+startBtn.setAttribute('disabled', true);
+
+const timer = document.querySelector('.timer');
+const iconError = './img/icon-error.svg'
+
+
+const timerValues = {
+  days: timer.querySelector('[data-days]'),
+  hours: timer.querySelector('[data-hours]'),
+  minutes: timer.querySelector('[data-minutes]'),
+  seconds: timer.querySelector('[data-seconds]'),
+};
+
+let userSelectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    checkData(selectedDates[0]);
+  },
+};
+flatpickr(datetimePicker, options);
+
+const izitoastOptions = {
+  title: 'Error',
+  message: 'Illegal operation',
+  position: 'topRight',
+  backgroundColor: '#EF4040',
+  theme: 'dark',
+  icoтUrl: iconError,
+};
+const checkData = data => {
+  if (data.getTime() > Date.now()) {
+    userSelectedDate = data;
+    startBtn.removeAttribute('disabled');
+  } else {
+    startBtn.setAttribute('disabled', true);
+    izitoast.error(izitoastOptions);
+  }
+};
+
+const addLeadingZero = value => {
+  return String(value).padStart(2, '0');
+};
+
+const setValues = () => {
+  let { days, hours, minutes, seconds } = convertMs(
+    userSelectedDate.getTime() - Date.now()
+  );
+  if (days < 0 && hours < 0 && minutes < 0 && seconds < 0) {
+    return null;
+  }
+
+  timerValues.days.textContent = addLeadingZero(days);
+  timerValues.hours.textContent = addLeadingZero(hours);
+  timerValues.minutes.textContent = addLeadingZero(minutes);
+  timerValues.seconds.textContent = addLeadingZero(seconds);
+  return true;
+};
+
+const stopTimer = intervalId => {
+  clearInterval(intervalId);
+};
+
+const start = () => {
+  datetimePicker.setAttribute('disabled', true);
+  startBtn.setAttribute('disabled', true);
+
+  setValues();
+  const timerIntervalId = setInterval(() => {
+    setValues() ? setValues() : stopTimer(timerIntervalId);
+  }, 1000);
+};
+
+startBtn.addEventListener('click', start);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,46 @@
+import iziToast from 'izitoast';
+
+const form = document.querySelector('.form');
+
+const getFulfilledPromise = delay => {
+  setTimeout(() => {
+    iziToast.success({
+      message: `✅ Fulfilled promise in ${delay}ms`,
+      position: 'topRight',
+      icon: '',
+      backgroundColor: '#59a10d',
+      theme: 'dark',
+    });
+  }, delay);
+};
+const getRejectedPromise = delay => {
+  setTimeout(() => {
+    iziToast.success({
+      message: `❌ Rejected promise in ${delay}ms`,
+      position: 'topRight',
+      icon: '',
+      backgroundColor: '#ef4040',
+      theme: 'dark',
+    });
+
+  }, delay);
+};
+
+const onFormSubmit = e => {
+  e.preventDefault();
+  const delay = document.querySelector('[name="delay"]').value;
+  const state = document.querySelector('input[name="state"]:checked').value;
+
+  switch (state) {
+    case 'fulfilled':
+      getFulfilledPromise(delay);
+      break;
+    case 'rejected':
+      getRejectedPromise(delay);
+      break;
+  }
+
+  form.reset();
+};
+
+form.addEventListener('submit', onFormSubmit);
